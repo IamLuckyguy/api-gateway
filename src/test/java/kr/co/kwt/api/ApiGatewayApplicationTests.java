8d1a4cd796cc08bc37bbfcecec4e058d3a2f1308
lucky <lucky@wemakeprice.com>
2024-10-03 15:36:00 +0900

    first commit

---
+++ src/test/java/kr/co/kwt/api/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kwt.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
